a225cc44807b72554755ddc66ef67309895cd78c
linggit90 <127336147+linggit90@users.noreply.github.com>
2023-06-24 10:29:24 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`http://todayshottrends.info/` + encodeURI(path as string)
+					`https://todayshottrends.info/how-to-make-cakes-with-a-rice-cooker/`
 				}`,
 			},
 		};
